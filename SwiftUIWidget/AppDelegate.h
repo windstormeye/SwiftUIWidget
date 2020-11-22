368331589670df901dbd10bbc8b52298da852fb2
wengpeijun <wengpeijun@bytedance.com>
2020-11-22 11:15:51 +0800

    Initial Commit

---
+++ SwiftUIWidget/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SwiftUIWidget
+//
+//  Created by 翁培钧 on 2020/11/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
